dc0991e860098f43889e0b070fbdd138d78e9803
siddhxrthh <ksiddharth127@gmail.com>
2022-08-08 22:26:28 -0300

    removed creds

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,4 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-  UserPoolId: "us-east-1_m6LuHVf0a",
-  ClientId: "62n91akuolkhauklj1pfpgkdjv",
-};
+import { poolData } from "../configurations";
 
 export default new CognitoUserPool(poolData);
